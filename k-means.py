b10e6dffb2f92f8193a1d2a15cdddb72a4529be0
chen <yz_chen1999@163.com>
2019-09-10 11:01:16 +0800

    edit k-means

--- k-means.py
+++ k-means.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 
 def loadData(fpath):
     mat = []
+    labels = []
     with open(fpath, 'r') as f:
         for line in f.readlines():
+            label = line.split()[-1]
             line = line.split()[:-1]
             line = [float(num) for num in line]
             mat.append(line)
+            labels.append(int(label))
 
     mat = np.array(mat)
-    return mat
+    return mat, labels
 
 def choose2rand(m):
     p1 = m
@@ -21,54 +24,108 @@ def choose2rand(m):
 
     return p1, p2
 
-def k_means(inX, maxIter, K = 2):
+def k_means(inX, maxIter, K = 2, looptimes = 1):
     m = len(inX)
-    labelMat = [1 for i in range(m)]
-    p1, p2 = choose2rand(m)
-    p1 = inX[p1]
-    p2 = inX[p2]
+    times = 0
+    labelList = []
+    costList  = []
+    labelMat  = [1 for i in range(m)]
+    p0, p1    = choose2rand(m)
+    p0        = inX[p0]
+    p1        = inX[p1]
 
 
-    for i in range(maxIter):
-        for j in range(m):
-            d1 = np.multiply(inX[j] - p1, inX[j] - p1).sum()
-            d2 = np.multiply(inX[j] - p2, inX[j] - p2).sum()
+    for l in range(looptimes):
+        p0, p1 = choose2rand(m)
+        p0 = inX[p0]
+        p1 = inX[p1]
+        templabel = [0 for i in range(m)]
+        for i in range(maxIter):
+            for j in range(m):
+                d0 = np.multiply(inX[j] - p0, inX[j] - p0).sum()
+                d1 = np.multiply(inX[j] - p1, inX[j] - p1).sum()
 
-            if (d1 > d2):
-                labelMat[j] = 2
+                if (d0 > d1):
+                    templabel[j] = 1
 
-        p1_sum = np.zeros(inX[0].shape)
-        p1_num = 0
-        p2_sum = np.zeros(inX[0].shape)
-        p2_num = 0
-        for p in range(m):
-            if (labelMat[p] == 1):
-                p1_sum += inX[p]
-                p1_num += 1
+            p1_sum = np.zeros(inX[0].shape)
+            p1_num = 0
+            p2_sum = np.zeros(inX[0].shape)
+            p2_num = 0
+            for p in range(m):
+                if (templabel[p] == 0):
+                    p1_sum += inX[p]
+                    p1_num += 1
 
+                else:
+                    p2_sum += inX[p]
+                    p2_num += 1
+
+            p0 = p1_sum / p1_num
+            p1 = p2_sum / p2_num
+
+        cost = 0
+        for i in range(m):
+            if (templabel[i] == 0):
+                d = np.multiply(inX[i] - p0, inX[i] - p0).sum()
             else:
-                p2_sum += inX[p]
-                p2_num += 1
+                d = np.multiply(inX[i] - p1, inX[i] - p1).sum()
 
-        p1 = p1_sum / p1_num
-        p2 = p2_sum / p2_num
+            cost += d
 
-    return labelMat
+        labelList.append(templabel)
+        costList.append(cost)
+
+    mincost = costList[0]
+    minIndex = 0
+    for i in range(len(costList)):
+        if (costList[i] < mincost):
+            minIndex = i
+
+    labelMat = labelList[minIndex]
+
+    return labelMat, p0, p1
 
 def draw(fpath):
-    inX = loadData(fpath)
-    labelMat = k_means(inX, 100)
+    inX, labels = loadData(fpath)
+    labelMat, p1, p2 = k_means(inX, 100, 2, 4)
     fig = plt.figure()
-    ax1 = fig.add_subplot(1, 1, 1)
     m = len(labelMat)
+    error = 0
+    rate = 0
+
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(p1[0], p1[1], color = 'k', s = 50)
+    ax.scatter(p2[0], p2[1], color = 'k', s = 50)
 
     for i in range(m):
+        if (labelMat[i] != labels[i]):
+            error += 1
         if (labelMat[i] == 1):
-            ax1.scatter(inX[i][0], inX[i][1], color = 'r')
+            ax.scatter(inX[i][0], inX[i][1], color='r', s = 20)
 
         else:
-            ax1.scatter(inX[i][0], inX[i][1], color = 'b')
+            ax.scatter(inX[i][0], inX[i][1], color='b', s = 20)
+
+    #print(labelMat)
+    #print(labels)
+    plt.show()
+    rate = error / m
+    if (rate > 0.5):
+        rate = 1 - rate
+    print(rate)
+    '''
+    for j in range(4):
+        labelMat = labelList[j]
+        axj = fig.add_subplot(2, 2, j + 1)
+        for i in range(m):
+            if (labelMat[i] == 1):
+                axj.scatter(inX[i][0], inX[i][1], color = 'r')
+
+            else:
+                axj.scatter(inX[i][0], inX[i][1], color = 'b')
 
     plt.show()
+    '''
 
 draw("C:\\Users\\91969\\Desktop\\testSet.txt")
